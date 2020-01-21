aa2df30b73dc2b982529ff092673189f3c1923cf
Victor <victor.fan.wang@gmail.com>
2020-01-21 00:22:02 -0800

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '766px',
+      lg: '978px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+};
